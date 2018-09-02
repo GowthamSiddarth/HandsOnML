4d267b5197cc3e8a51a85f763f8fdc724e37535f
Gowtham Behara <gothsiddu@gmail.com>
2018-09-02 10:35:17 +0530

    Define vars for housing dataset url

--- Housing.py
+++ Housing.py
@@ -3,3 +3,9 @@ import pandas as pd
 import tarfile
 
 from six.moves import urllib
+
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
+HOUSING_PATH = "datasets/housing"
+HOUSING_URL = DOWNLOAD_ROOT + HOUSING_PATH + "/housing.tgz"
+
+
