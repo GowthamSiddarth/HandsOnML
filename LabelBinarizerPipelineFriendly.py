d68d6b756a48c7173130d4716605724495a1b9d9
Gowtham Behara <gothsiddu@gmail.com>
2018-10-07 22:37:05 +0530

    Create custom LabelBinarizer to fit the model

---
+++ LabelBinarizerPipelineFriendly.py
@@ -0,0 +1,13 @@
+from sklearn.preprocessing import LabelBinarizer
+
+
+class LabelBinarizerPipelineFriendly(LabelBinarizer):
+    def fit(self, X, y=None):
+        """this would allow us to fit the model based on the X input."""
+        super(LabelBinarizerPipelineFriendly, self).fit(X)
+
+    def transform(self, X, y=None):
+        return super(LabelBinarizerPipelineFriendly, self).transform(X)
+
+    def fit_transform(self, X, y=None):
+        return super(LabelBinarizerPipelineFriendly, self).fit(X).transform(X)
